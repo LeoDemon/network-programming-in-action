712c745d2383d08862ba9ddf6e78eaffec4f3f02
DemonLee <jwkljh@aliyun.com>
2022-08-06 17:41:51 +0800

    coding for chapter-11

---
+++ ch11/shutdown_client.c
@@ -0,0 +1,91 @@
+//
+// Created by DemonLee on 8/5/22.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <arpa/inet.h>
+#include <strings.h>
+#include <unistd.h>
+#include "common/log.h"
+#include "common/general.h"
+
+int main(int argc, char **argv) {
+    if (argc < 3) {
+        error_handling(stderr, "Usage: shutdown_client <IP> <Port>");
+    }
+
+    struct sockaddr_in server_addr;
+    bzero(&server_addr, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(atoi(argv[2]));
+    inet_pton(AF_INET, argv[1], &server_addr.sin_addr);
+
+    int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    int ret = connect(socket_fd, (struct sockaddr *) &server_addr, sizeof(server_addr));
+    if (ret < 0) {
+        error_handling(stderr, "client socket connect failed");
+    }
+
+    char send_line[MAX_LINE], recv_line[MAX_LINE + 1];
+    ssize_t n;
+
+    fd_set read_mask;
+    fd_set all_reads;
+
+    FD_ZERO(&all_reads);
+    FD_SET(0, &all_reads);
+    FD_SET(socket_fd, &all_reads);
+
+    for (;;) {
+        read_mask = all_reads;
+        int rc = select(socket_fd + 1, &read_mask, NULL, NULL, NULL);
+        if (rc < 0) {
+            error_handling(stderr, "select failed");
+        }
+
+        if (FD_ISSET(socket_fd, &read_mask)) {
+            n = read(socket_fd, recv_line, MAX_LINE);
+            if (n < 0) {
+                error_handling(stderr, "read data error");
+            } else if (n == 0) {
+                error_handling(stderr, "server terminated");
+            }
+            recv_line[n] = '\0';
+            fputs(recv_line, stdout);
+            fputs("\n", stdout);
+        }
+
+        if (FD_ISSET(0, &read_mask)) {
+            if (fgets(send_line, MAX_LINE, stdin) == NULL) {
+                continue;
+            }
+            if (strncmp(send_line, "shutdown", 8) == 0) {
+                FD_CLR(0, &all_reads);
+                if (shutdown(socket_fd, 1)) {
+                    error_handling(stderr, "shutdown failed");
+                }
+            } else if (strncmp(send_line, "close", 5) == 0) {
+                FD_CLR(0, &all_reads);
+                if (close(socket_fd)) {
+                    error_handling(stderr, "close failed");
+                }
+                sleep(6);
+                exit(0);
+            } else {
+                size_t i = strlen(send_line);
+                if (send_line[i - 1] == '\n') {
+                    send_line[i - 1] = '\0';
+                }
+                printf("now sending: [%s]\n", send_line);
+                ssize_t rt = write(socket_fd, send_line, strlen(send_line));
+                if (rt < 0) {
+                    error_handling(stderr, "write data error");
+                }
+                printf("send bytes: %zu\n", rt);
+            }
+        }
+    }
+
+    return 0;
+}
---
+++ ch11/shutdown_server.c
@@ -0,0 +1,82 @@
+//
+// Created by DemonLee on 8/5/22.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <arpa/inet.h>
+#include <strings.h>
+#include <unistd.h>
+#include "common/log.h"
+#include "common/general.h"
+
+static int count;
+
+static void sig_int(int signo) {
+    printf("received signal: %d\n", signo);
+    printf("received %d datagrams.\n", count);
+    exit(0);
+}
+
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        error_handling(stderr, "Usage: shutdown_server <Port>");
+    }
+
+    struct sockaddr_in server_addr;
+    bzero(&server_addr, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(atoi(argv[1]));
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    int listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+    int ret = bind(listen_fd, (struct sockaddr*)&server_addr, sizeof(server_addr));
+    if (ret < 0) {
+        error_handling(stderr, "server socket bind failed");
+    }
+
+    ret = listen(listen_fd, LISTEN_QUEUE);
+    if (ret < 0) {
+        error_handling(stderr, "server socket listen failed");
+    }
+
+    signal(SIGINT, sig_int);
+    signal(SIGPIPE, sig_int);
+
+    struct sockaddr_in client_addr;
+    socklen_t client_len = sizeof(client_addr);
+
+    int conn_fd = accept(listen_fd, (struct sockaddr*)&client_addr, &client_len);
+    if (conn_fd < 0) {
+        error_handling(stderr, "server accept client socket failed");
+    }
+
+    char message[MAX_LINE];
+    count = 0;
+
+    for(;;) {
+        ssize_t n = read(conn_fd, message, MAX_LINE);
+        if (n < 0) {
+            error_handling(stderr, "read message error");
+        }else if (n == 0) {
+            error_handling(stderr, "client closed");
+        }
+        message[n] = '\0';
+        printf("received %zu bytes: [%s]\n", n, message);
+        count++;
+
+        char send_line[MAX_LINE];
+        sprintf(send_line, "message [%s] received.", message);
+
+        sleep(5);
+
+        printf("sending bytes: %s\n", send_line);
+        ssize_t write_nc = send(conn_fd, send_line, strlen(send_line), 0);
+        printf("send %zu bytes: %s\n", write_nc, send_line);
+        if (write_nc < 0) {
+            error_handling(stderr, "send message error");
+        }
+    }
+
+    return 0;
+}
